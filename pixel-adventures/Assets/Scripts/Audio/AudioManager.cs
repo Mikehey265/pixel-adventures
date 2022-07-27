f0649a95777c6dd39573fedea2b72fc041a2b38e
Mikehey265 <mike.programming265@gmail.com>
2022-07-28 00:06:46 +0200

    AudioManager added SerializeField

--- pixel-adventures/Assets/Scripts/Audio/AudioManager.cs
+++ pixel-adventures/Assets/Scripts/Audio/AudioManager.cs
@@ -6,12 +6,13 @@ public class AudioManager : Singleton<AudioManager>
 {
     [SerializeField] AudioSource audioSource;
     [SerializeField] public AudioClip[] enemySFX;
+    [SerializeField] float audioClipLength = 0.3f;
 
     private void Start() {
         audioSource = GetComponent<AudioSource>();
     }
 
     public void PlayEnemySFX(AudioClip audioClip){
-        audioSource.PlayOneShot(audioClip, 0.3f);
+        audioSource.PlayOneShot(audioClip, audioClipLength);
     }
 }
